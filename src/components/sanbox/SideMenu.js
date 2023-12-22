d7ae588e2942668f0d2b140b997f944f2434fdb6
WU FAQIANG <zl837413707@gmail.com>
2023-12-23 01:24:51 +0900

    2023/12/23

--- src/components/sanbox/SideMenu.js
+++ src/components/sanbox/SideMenu.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import {
   DesktopOutlined,
   FileOutlined,
@@ -11,27 +12,45 @@ import { Layout, Menu } from 'antd';
 import './index.css'
 const { Sider } = Layout;
 
-const items = [
-  { key: '/home', label: 'ホーム', icon: <UserOutlined />, },
-  {
-    key: '/user-manage', label: 'ユーザー管理', icon: <TeamOutlined />, children: [
-      { key: '/user-manage/list', label: 'ユーザーリスト', icon: <PieChartOutlined /> }
-    ]
-  },
-  {
-    key: '/right-manage', label: '権限管理', icon: <TeamOutlined />, children: [
-      { key: '/right-manage/role/list', label: 'ロールリスト', icon: <PieChartOutlined /> },
-      { key: '/right-manage/right/list', label: '権限リスト', icon: <PieChartOutlined /> }
-    ]
-  },
-];
+const iconList = {
+  '/home': <PieChartOutlined />,
+  '/user-manage': <UserOutlined />,
+  '/right-manage': <TeamOutlined />,
+}
 
 export default function SideMenu() {
   const navigate = useNavigate();
+  const [menu, setMenu] = useState([])
+  useEffect(() => {
+    const getData = async () => {
+      await axios.get('http://localhost:8100/rights?_embed=children').then((res) => {
+        // 为了删除空的children和rightId,真正做后端的时候可以删除下面的方法
+        res.data.forEach((res) => {
+          // 后端本身没有icon的信息,根据唯一的key值然后自定义一个iconList对象,每次遍历的时候让icon等于上面对象里的icon组件(成为一个新的数组使用)
+          res.icon = iconList[res.key]
+          if (res.children.length > 0) {
+            res.children.forEach((item) => {
+              delete item["rightId"]
+            })
+            //过滤下数组,有pagepermisson的数据代表着是菜单栏可以显示的权限(导致没有权限的数据显示不出来,后面有问题想办法解决！！！！！！！！)
+            const newChildren = res.children.filter((item) => {
+              return item.pagepermisson === 1;
+            })
+            res["children"] = newChildren
+          } else {
+            delete res["children"]
+          }
+        })
+        console.log(res.data);
+        setMenu(res.data)
+      })
+    }
+    getData()
+  }, [])
   return (
     <Sider trigger={null} collapsible className="side" >
       <div className="logo" >ニュース管理システム</div>
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} onClick={(items) => { navigate(items.key) }} />
+      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={menu} onClick={(menu) => { navigate(menu.key) }} />
     </Sider>
   )
 }
--- src/views/sanbox/home/Home.js
+++ src/views/sanbox/home/Home.js
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 
 export default function Home() {
-  useEffect(() => {
-    const getData = async () => {
-      const response = await axios.get('http://localhost:8100/posts')
-      console.log(response.data)
-    }
-    getData()
-  }, [])
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const response = await axios.get('http://localhost:8100/posts')
+  //     console.log(response.data)
+  //   }
+  //   getData()
+  // }, [])
   return (
     <div>Home</div>
   )
